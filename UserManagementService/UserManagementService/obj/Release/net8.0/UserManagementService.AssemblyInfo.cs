4019bada7fb8209f5b3ede28006646df8f92179d
azur-14 <hieungan0906@gmail.com>
2025-03-28 12:59:28 +0700

    hientai

--- UserManagementService/UserManagementService/obj/Release/net8.0/UserManagementService.AssemblyInfo.cs
+++ UserManagementService/UserManagementService/obj/Release/net8.0/UserManagementService.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UserManagementService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8e3aefb863ec8fad89b055e9d412943a47d696f5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+65418d253e23590f3de75b1bc2912164ffd1e0e4")]
 [assembly: System.Reflection.AssemblyProductAttribute("UserManagementService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UserManagementService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
