699ead8265e305a743f208ae0000fe9cc75a407b
azur-14 <hieungan0906@gmail.com>
2025-04-16 15:55:24 +0700

    vi du ve responsive danentang cho Login_SignUp_Screen.dart

--- frontend/lib/constants/colors.dart
+++ frontend/lib/constants/colors.dart
@@ -8,4 +8,17 @@ class AppColors {
   static const Color textPrimary = Color(0xFF212121);
   static const Color textSecondary = Color(0xFF757575);
   static const Color divider = Color(0xFFBDBDBD);
+
+  // Màu chính (Primary Color) - dùng cho nền Card
+  static const Color brandPrimary = Color(0xFF211463);
+
+  // Màu phụ (Secondary Color)
+  static const Color brandSecondary = Color(0xFF642FBF);
+
+  // Màu nhấn (Accent Color)
+  static const Color brandAccent = Color(0xFF211463);
+
+  // Một số màu phụ trợ khác nếu cần
+  static const Color brandLight = Color(0xFFF2E7FE);
+  static const Color brandDark = Color(0xFF1C0D4F);
 }
--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -21,7 +21,6 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       debugShowCheckedModeBanner: false,
-
       home: LoginSignupScreen(),
     );
   }
