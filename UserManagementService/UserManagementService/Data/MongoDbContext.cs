01f7a2ac04a8b22803358ca6fb61f7d417e36cb2
azur-14 <hieungan0906@gmail.com>
2025-04-18 02:17:53 +0700

    cap nhat mongo

---
+++ UserManagementService/UserManagementService/Data/MongoDbContext.cs
@@ -0,0 +1,19 @@
+﻿using MongoDB.Driver;
+using Microsoft.Extensions.Configuration;
+using UserManagementService.Models;
+
+namespace UserManagementService.Data
+{
+    public class MongoDbContext
+    {
+        private readonly IMongoDatabase _database;
+
+        public MongoDbContext(IConfiguration config)
+        {
+            var client = new MongoClient(config.GetConnectionString("MongoDb"));
+            _database = client.GetDatabase("UserService");
+        }
+
+        public IMongoCollection<User> Users => _database.GetCollection<User>("Users");
+    }
+}
