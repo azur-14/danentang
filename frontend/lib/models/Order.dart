4c7907ab5282ed804ed54a4d736413fdac9ee5f2
HuyenDieu1 <131192460+HuyenDieu1@users.noreply.github.com>
2025-05-16 02:10:38 +0700

    order

--- frontend/lib/models/Order.dart
+++ frontend/lib/models/Order.dart
@@ -13,12 +13,9 @@ class Order {
   final double discountAmount;
   final String? couponCode;
   final int loyaltyPointsUsed;
-<<<<<<< HEAD
   String status; // Removed 'final'
-=======
   final int loyaltyPointsEarned; // ✅ mới thêm
   final String status;
->>>>>>> bf92b695419ac74d1dad522fe935bf06c8b4599c
   final List<OrderStatusHistory> statusHistory;
   final DateTime createdAt;
   DateTime updatedAt; // Removed 'final'
@@ -41,21 +38,21 @@ class Order {
   });
 
   factory Order.fromJson(Map<String, dynamic> json) => Order(
-<<<<<<< HEAD
+
     id: json['_id'] as String,
     userId: json['userId'] as String,
     orderNumber: json['orderNumber'] as String,
     shippingAddress: ShippingAddress.fromJson(json['shippingAddress'] as Map<String, dynamic>),
     items: (json['items'] as List<dynamic>)
         .map((e) => OrderItem.fromJson(e as Map<String, dynamic>))
-=======
+
     id: json['_id'] ?? '', // fallback nếu tạo mới
     userId: json['userId'],
     orderNumber: json['orderNumber'],
     shippingAddress: ShippingAddress.fromJson(json['shippingAddress']),
     items: (json['items'] as List)
         .map((e) => OrderItem.fromJson(e))
->>>>>>> bf92b695419ac74d1dad522fe935bf06c8b4599c
+
         .toList(),
     totalAmount: (json['totalAmount'] as num).toDouble(),
     discountAmount:
