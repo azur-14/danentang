8877a96a4c6347f7530413603d6b18e7b0eea02c
azur-14 <hieungan0906@gmail.com>
2025-05-19 16:11:13 +0700

    push update server

--- TestRabbit/Program.cs
+++
@@ -1,7 +0,0 @@
-﻿using RabbitMQ.Client;
-
-var factory = new ConnectionFactory() { HostName = "localhost" };
-using var conn = factory.CreateConnection();
-using IModel channel = conn.CreateModel();
-
-Console.WriteLine("✅ IModel OK!");
