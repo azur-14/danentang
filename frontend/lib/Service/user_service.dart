082eb4b5ac3f230cbb619096d3aee3d4a4044087
azur-14 <hieungan0906@gmail.com>
2025-05-19 18:42:46 +0700

    doi link product va user

--- frontend/lib/Service/user_service.dart
+++ frontend/lib/Service/user_service.dart
@@ -15,9 +15,9 @@ class UserService {
 
   UserService({
     // use 10.0.2.2 on Android emulator, localhost on web/desktop
-    String authBase = kIsWeb ? 'http://localhost:5012/api/auth' : 'http://10.0.2.2:5012/api/auth',
-    String userBase = kIsWeb ? 'http://localhost:5012/api/user' : 'http://10.0.2.2:5012/api/user',
-    String chatBase =  kIsWeb ? 'http://localhost:5012/api/complaint' : 'http://10.0.2.2:5012/api/complaints',
+    String authBase = 'https://usermanagementservice-production-697c.up.railway.app/api/auth',
+    String userBase = 'https://usermanagementservice-production-697c.up.railway.app/api/user',
+    String chatBase = 'https://usermanagementservice-production-697c.up.railway.app/api/complaint',
 
   })  : _authBase = authBase,
         _userBase = userBase,
